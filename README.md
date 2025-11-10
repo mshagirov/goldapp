2c42bcb08d60b6f4b4653c86760af404ef002a6c
mshagirov <muratshagirov@gmail.com>
2025-11-10 16:10:29 +0800

    ldap notes

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # goldap : go LDAP application
 
 > LDAP TUI in go
+
+## LDAP
+
+- [LDAP in Go](https://cybernetist.com/2020/05/18/getting-started-with-go-ldap/)
+- go-ldap:
+  - [repo](https://github.com/go-ldap/ldap)
+  - [Docs](https://pkg.go.dev/github.com/go-ldap/ldap)
+
+## SQLite DB Notes
+
+> Not needed yet
+
+### Turso SQL DB
+
+- [Quickstart guide](https://docs.turso.tech/tursodb/quickstart)
+- Installation:
+
+```sh
+curl --proto '=https' \
+--tlsv1.2 -LsSf \
+https://github.com/tursodatabase/turso/releases/latest/download/turso_cli-installer.sh | sh
+```
+
+### goose
+
+- [goose documentation](https://pressly.github.io/goose/documentation/annotations/)
+
+```sh
+go install github.com/pressly/goose/v3/cmd/goose@latest
+```
---
+++ tui.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	cursor   int
+	choices  []string
+	selected map[int]struct{}
+}
+
+func initialModel() model {
+	return model{
+		choices: []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
+
+		// A map which indicates which choices are selected. We're using
+		// the map like a mathematical set. The keys refer to the indexes
+		// of the `choices` slice, above.
+		selected: make(map[int]struct{}),
+	}
+}
+
+func NewInitialModel() *tea.Program {
+	p := tea.NewProgram(initialModel())
+	return p
+}
+
+func (m model) Init() tea.Cmd {
+	return tea.SetWindowTitle("Grocery List")
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+
+			for k := range m.selected {
+				fmt.Printf("%s\n", m.choices[k])
+			}
+			// fmt.Println("Bye!")
+			return m, tea.Quit
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "What should we buy at the market?\n\n"
+
+	for i, choice := range m.choices {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
+
+		checked := " "
+		if _, ok := m.selected[i]; ok {
+			checked = "x"
+		}
+
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	s += "\nPress q to quit.\n"
+
+	return s
+}
